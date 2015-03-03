51bfc459400a057aabfc09a95efb4cb495e2a7c8
Allan Stewart <stewart.allan@gmail.com>
2015-03-02 22:49:49 -0700

    File system example.

---
+++ files.js
@@ -0,0 +1,12 @@
+var fileSystem = require('fs');
+var fileCount = 0;
+var dirCount = 0;
+fileSystem.readdir('.', function (error, files) {
+	if (error) return console.error(error);
+	files.map(function (file) {
+		var stat = fileSystem.statSync(file);
+		if (stat.isFile()) fileCount++;
+		if (stat.isDirectory()) dirCount++;
+	});
+	console.log('%d files and %d directories', fileCount, dirCount);
+});
