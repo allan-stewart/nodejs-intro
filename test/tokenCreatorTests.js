9c380774b6859e141e7774161bdfc896983f186d
Allan Stewart <stewart.allan@gmail.com>
2015-03-04 22:08:28 -0700

    Additional refactoring; renaming.

--- test/tokenCreatorTests.js
+++ test/tokenCreatorTests.js
@@ -1,4 +1,4 @@
-var token = require('../tokenCreator.js');
+var tokenCreator = require('../tokenCreator.js');
 var assert = require('assert');
 
 describe('makeToken', function () {
@@ -6,6 +6,6 @@ describe('makeToken', function () {
 		var key = new Buffer('6b334d656e2a702f322e336a34616242', 'hex');
 		var data = ['user123', '2015-03-13T05:30:00Z'];
 		var expected = 'user123|2015-03-13T05:30:00Z|17ea924623dd6eef23d2faeb3ccd293e';
-		assert.equal(token.makeToken(key, data), expected);
+		assert.equal(tokenCreator.createToken(key, data), expected);
 	});
 });
--- tokenCreator.js
+++ tokenCreator.js
@@ -1,6 +1,6 @@
 var aesCmac = require('node-aes-cmac').aesCmac;
 
-exports.makeToken = function (key, data) {
+exports.createToken = function (key, data) {
 	var message = data.join('|');
 	var cmac = aesCmac(key, message);
 	return message + '|' + cmac;
